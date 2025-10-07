589097e1476a562bd27d5ebfdd8133cde91fb02c
selgiel <112975437+selgiel@users.noreply.github.com>
2025-10-08 02:48:29 +0800

    2a(viii)-2a(xi) responsiveness
    Added media tags/ top navigation bar for smaller screens and adjusted designs for varying screen sizes

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\gisel\OneDrive - SUSS University\Y2S1\ICT239\TMA\TMA01\venv
+set VIRTUAL_ENV=C:\Users\gisel\OneDrive\Documents\GitHub\TMA01\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
