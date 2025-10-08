841f878da3199940500fcc395449b3fd5b5fdd5a
selgiel <112975437+selgiel@users.noreply.github.com>
2025-10-09 02:24:52 +0800

    Created folders to separate different parts
    Shifted all the files under Q2a and replicated files for part b (Q2b)

---
+++ Q2b/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, url_for, redirect
+from books import all_books  # Assume all_books is a list of book dicts in books.py
+
+app = Flask(__name__, static_folder='static')  # Make sure this points to 'static'
+
+for i, b in enumerate(all_books):
+    b['id'] = i + 1
+
+# Utility to fetch first and last paragraphs of description
+def get_first_last_paragraph(description):
+    paragraphs = [p for p in description if p]
+    if len(paragraphs) >= 2:
+        return paragraphs[0], paragraphs[-1]
+    elif paragraphs:
+        return paragraphs[0], ''
+    else:
+        return '', ''
+
+@app.route('/', methods=['GET', 'POST'])
+def book_titles():
+    category = request.form.get('category', 'All')
+    if category == 'All':
+        books_filtered = sorted(all_books, key=lambda x: x['title'])
+    else:
+        books_filtered = sorted([b for b in all_books if b['category'] == category], key=lambda x: x['title'])
+
+    for b in books_filtered:
+        b['first_para'], b['last_para'] = get_first_last_paragraph(b['description'])
+
+    categories = ['All', 'Children', 'Teens', 'Adult']
+    return render_template('book_titles.html', books=books_filtered, categories=categories, selected=category)
+
+def get_book(book_id: int):
+    return next((b for b in all_books if b.get("id") == book_id), None)
+
+@app.route('/books/<int:book_id>')
+def book_details(book_id):
+    book = get_book(book_id)
+    if not book:
+        return redirect(url_for('book_titles'))
+    return render_template('book_detail.html', book=book)
+
+if __name__ == '__main__':
+    app.run(debug=True)
